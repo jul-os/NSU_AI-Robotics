9a6ad8b8941d0ce1365591553de8df67c072c036
jul-osn <julia.osnovskaya@mail.ru>
2025-03-17 19:17:24 +0700

    update

--- imper/2-p-24-10-mar/2/main.c
+++ imper/2-p-24-10-mar/2/main.c
@@ -36,6 +36,28 @@ int main()
     assert(pdiv(0, 1) == 0);
     assert(pdiv(1, 1) == 1);
 
+
+    MOD = 101;
+    assert(pnorm(1) == 1);
+    assert(pnorm(255) == 53);
+    assert(pnorm(-13) == 88);
+    assert(pnorm(-101) == 0);
+    assert(pnorm(421412888) == 84);
+    assert(padd(123, 241) == 61);
+    assert(padd(1, 100) == 0);
+    assert(psub(10, 5) == 5);
+    assert(psub(-1077, 5) == 29);
+    assert(psub(100, 1) == 99);
+    assert(pmul(1, 1) == 1);
+    assert(pmul(1, 0) == 0);
+    assert(pmul(321, 211) == 61);
+    assert(pmul(101, 101) == 0);
+    assert(pdiv(0, 5) == 0);
+    assert(pdiv(101, 101) == 0);
+    assert(pdiv(103312, 121) == 55);
+    assert(pdiv(1, 5) == 81);
+    assert(pdiv(1, 101) == 0);
+
     MOD = 1000000000 - 1;
     assert(pnorm(0) == 0);
     assert(pnorm(5) == 5);
@@ -53,6 +75,8 @@ int main()
     assert(pmul(1, 1) == 1);
     assert(pmul(2, 3) == 6);
     assert(pdiv(0, 5) == 0);
+    assert(pdiv(999999998, 999999998) == 1);
+    assert(pdiv(999999999, 118) == 0);
 
     return 0;
 }
